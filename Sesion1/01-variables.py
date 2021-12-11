555bfe3bfc43e5294599c91c6f796ab0f38a8872
Alan Crisanto <alanvcrisanto@gmail.com>
2021-12-10 20:15:59 -0500

    chore: declaracion de variables

---
+++ Sesion1/01-variables.py
@@ -0,0 +1,38 @@
+# variable númerica
+numero = 0
+numero2 = 10.5
+
+
+# variables de texto o string
+nombre = "Eduardo"
+apellido = 'Ramiro'
+
+html="""<html>
+<p>
+</p>"""
+
+html='''<html>
+<p>
+</p>'''
+
+print("holaa :)")
+print(type(nombre))
+print(type(numero))
+
+soltero = True
+calor = False
+print(type(soltero))
+
+# variables de varios valores
+
+edades = [10, 12, 40, 60, "Alan", 12.4, False, [1, 2]]
+
+print(type(edades))
+
+# JSON = diccionario
+
+curso = {
+    "nombre": "Backend",
+    "npmbre": "Frontend",
+    "dificultad":"dificil"
+}
