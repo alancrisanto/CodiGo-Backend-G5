ffc7b146e2f67def9aa22d9a1e0d03e02eed9fa6
alancrisanto <alanvcrisanto@gmail.com>
2022-01-05 18:24:12 -0500

    Update

--- Sesion01/inicios-flask.py
+++ Sesion01/inicios-flask.py
@@ -1,8 +1,23 @@
 from flask import Flask
 
-class Persona():
+# en python tenemos varias variables que son de uso propio de python no podemos modificar ni alterar
+# __main__ > esta variable sirve para indicar si estamos en el archivo principal del proyecto
 
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
+app = Flask(__name__)
 
+#el decorador sirve para usar el método de una clase pero implementandolo en una función
+@app.route('/') # <= este es el decorador
+def inicio():
+    return "Bienvenido a mi API"
+
+@app.route("/bienvenida")
+def bienvenida():
+    return "Te doy la bienvenida a mi API"
+
+@app.route("/bienvenida/home")
+def bienvenido_home():
+    return "Te doy la bienvenida a API Home extraño"
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
