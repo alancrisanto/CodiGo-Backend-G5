13b3f2e46742bc7b4339beaefe61b359164f7900
alancrisanto <alanvcrisanto@gmail.com>
2022-01-14 18:33:10 -0500

    chore: Limpieza de rama

--- pruebas.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO CURSOS (nombre) values ('COMUNICACION')
-INSERT INTO CURSOS (nombre) values ('CTA')
-INSERT INTO CURSOS (nombre) values ('INGLES')
-INSERT INTO CURSOS (nombre) values ('FRENCH')
