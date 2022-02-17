88a207ca60a189de9fbf2329da756f820cff63fb
acrisanto <alanvcrisanto@gmail.com>
2022-02-17 18:28:40 -0500

    chore: inicio semana09

--- src/routes/categorias.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { crearCategoria, devolverCategorias } from "../controllers/categorias.controller.js";
-
-export const categoriaRouter = Router();
-
-categoriaRouter.route("/categoria").post(crearCategoria)
-
-categoriaRouter.route("/categorias").get(devolverCategorias)
-
